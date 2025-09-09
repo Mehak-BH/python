7e3164ad9f0db3e7b6542edcf3f4fff00c0858b2
Mehak Bhutani <135850630+Mehak-BH@users.noreply.github.com>
2025-09-09 13:40:38 +0530

    Update 27.py

--- 27.py
+++ 27.py
@@ -4,5 +4,6 @@ print(f"the list of inputs : {my_list}")
 int_list = list(map(int, input("enter numbers seperated by spaces : ").split()))
 print(f"the list of integers : {int_list}")
 
-print("this program is written and excecuted by MEHAK BHUTANI with ERP 0231BCA063")
+print("This code is written and excecuted by MEHAK BHUTANI with ERP 0231BCA063")
+
 
