39d2acc86a2c190cd8b26062e2a0e4dad9692ec5
Mehak Bhutani <135850630+Mehak-BH@users.noreply.github.com>
2025-09-10 10:07:07 +0530

    Add files via upload

---
+++ 54.py
@@ -0,0 +1,10 @@
+def count_up_to(max):
+    count=1
+    while count<=max:
+        yield count
+        count+=1
+
+for num in count_up_to(5):
+    print(num)
+
+print("This program is written and excecuted by MEHAK BHUTANI with ERP 0231BCA063")
